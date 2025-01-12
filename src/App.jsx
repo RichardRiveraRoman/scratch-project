6f78ae82c1c6302e8893bc5a3a4074f1062d6840
ssheikhi <fx.sheikhi@gmail.com>
2025-01-12 03:30:41 +0100

    Modify the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/login';
 import Signup from './components/signup';
-import { useState } from 'react';
 import healthLogo from './assets/health_logo.png';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <img src={healthLogo} className='logo health' alt='Health logo' />
-      </div>
-      <h1>Health App</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Log in to learn more</p>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      {/* Define Routes */}
+      <Routes>
+        {/* Route for the logo page */}
+        <Route
+          path='/'
+          element={
+            <div>
+              <div>
+                <img
+                  src={healthLogo}
+                  className='logo health'
+                  alt='Health logo'
+                />
+              </div>
+              <h1>Health App</h1>
+              <Link to='/login' className='read-the-docs'>
+                Log in to learn more
+              </Link>
+            </div>
+          }
+        />
+        {/* Route for the login page */}
+        <Route path='/login' element={<Login />} />
+        {/* Route for the signup page */}
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
