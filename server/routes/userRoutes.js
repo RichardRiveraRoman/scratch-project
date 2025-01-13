bc1002f36e5d554d347a8e30f2bf2b41b132ca21
BoyuHu514 <boyuhu@example.com>
2025-01-13 11:14:57 -0500

    userRoutes authenticate

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
+import authenticate from '../middlewares/authenticate.js';
 const router = express.Router();
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/me', userController.getUserProfile);
-router.put('/me', userController.updateUserProfile);
+router.get('/me', authenticate, userController.getUserProfile);
+router.put('/me', authenticate, userController.updateUserProfile);
 
 
 export default router;
