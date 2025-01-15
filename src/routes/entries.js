63edd83993174f58f9f8bb87e9018a106c279d0b
Molly Josephson <josephson4@yahoo.com>
2025-01-15 13:44:00 -0800

    added handleSubmit functionality and final stylings

---
+++ src/routes/entries.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Entry = require('');//path to model/schema
+
+router.post('./components/card', async(req, res)=>{
+    try{
+        const {exercise, time, fluidIntake, hoursSlept} = req.body 
+        
+    }
+})
