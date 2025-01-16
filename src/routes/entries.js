082beda479e1576702db2b6ed63d2a34ceaebac6
Molly Josephson <josephson4@yahoo.com>
2025-01-15 16:39:11 -0800

    Added comments

--- src/routes/entries.js
+++ src/routes/entries.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Entry = require(''); //path to model/schema
 
+//post endpoint at components.card - might be wrong endpoint
 router.post('./components/card', async (req, res) => {
   try {
+    //Destructures the req.body object to extract the data sent in the body of the POST request
     const { exercise, time, fluidIntake, hoursSlept } = req.body;
-    const newEntry = { exercise, time, fluidIntake, hoursSlept };
+    //Creates a new object called newEntry with the data extracted from the request body.
+    const newEntry = new Entry({ exercise, time, fluidIntake, hoursSlept });
+    //persist newEntry obj to database
     await newEntry.save();
     res.status(201).json({ message: 'Entry saved successfully' });
   } catch (error) {
