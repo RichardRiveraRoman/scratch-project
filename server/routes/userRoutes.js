62b26dffca9599ac9344966f98cd324698c45653
Richard Rivera <richard.rivera.roman@gmail.com>
2025-01-15 21:16:46 -0500

    Enable JWT authentication

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
-import authenticate from '../middlewares/authenticate.js';
 const router = express.Router();
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-// router.get('/me', authenticate, userController.getUserProfile);
-// router.put('/me', authenticate, userController.updateUserProfile);
-router.get('/me', authenticate, userController.getUserInfo);
 
 export default router;
